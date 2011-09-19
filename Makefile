golems/ach

Updated makefiles to work with some of jon's make-common portability enhancements.
git-svn-id: file:///home/svn/humanoids/lib/ach/trunk@1411 4f504ea5-f356-0410-adec-f1b3679f3bb7

Makefile
@@ -2,7 +2,7 @@ PROJECT := ach
 
 VERSION := 0.20090820
 
-LIBFILES := libach
+SHAREDLIBS := ach
 
 BINFILES := achcat achpipe.bin ach
 

golems/joystick

git-svn-id: file:///home/svn/humanoids/drivers/js@1411 4f504ea5-f356-0410-adec-f1b3679f3bb7

Makefile
@@ -3,7 +3,7 @@
 VERSION := 0.0.2
 PROJECT := libjs
 
-LIBFILES := libjs.so
+SHAREDLIBS := js
 BINFILES := jsstart jsd jscat
 
 default: $(LIBFILES) $(BINFILES)